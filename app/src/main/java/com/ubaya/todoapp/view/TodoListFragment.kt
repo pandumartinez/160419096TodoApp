50338f1f26e77edff5a9a5c06c3303ef7b05e3cd
pandusanika <pandu.martinez24@gmail.com>
2023-05-16 19:11:17 +0700

    done coding

---
+++ app/src/main/java/com/ubaya/todoapp/view/TodoListFragment.kt
@@ -0,0 +1,61 @@
+package com.ubaya.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.ubaya.todoapp.R
+import com.ubaya.todoapp.viewmodel.ListTodoViewModel
+
+class TodoListFragment : Fragment() {
+    private lateinit var viewModel: ListTodoViewModel
+    private val todoListAdapter  = TodoListAdapter(arrayListOf(), {item -> viewModel.clearTask(item)})
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListTodoViewModel::class.java)
+        viewModel.refresh()
+
+        val recViewTodo = view.findViewById<RecyclerView>(R.id.recViewTodo)
+        recViewTodo.layoutManager = LinearLayoutManager(context)
+        recViewTodo.adapter = todoListAdapter
+
+        val fabAddTodo = view.findViewById<FloatingActionButton>(R.id.fabAddTodo)
+        fabAddTodo.setOnClickListener {
+            val action = TodoListFragmentDirections.actionCreateTodo()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        observeViewModel(view)
+    }
+
+    fun observeViewModel(view: View) {
+        viewModel.todoLD.observe(viewLifecycleOwner, Observer {
+            todoListAdapter.updateTodoList(it)
+
+            val txtEmpty = view.findViewById<FloatingActionButton>(R.id.txtEmpty)
+            if(it.isEmpty()) {
+                txtEmpty.visibility = View.VISIBLE
+            } else {
+                txtEmpty.visibility = View.GONE
+            }
+        })
+    }
+
+}
